1756d13cefc895dfb5d0c08a264e74c87ec067bc
WeipengHu111 <70785418+WeipengHu111@users.noreply.github.com>
2024-06-27 17:46:41 -0400

    hero_component_animation_V2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   const handleScroll = () => {
     const scrollPosition = window.pageYOffset;
     const windowHeight = window.innerHeight;
-    const textCenterPosition = windowHeight / 8;
+    const textCenterPosition = windowHeight / 2;
 
     if (scrollPosition < textCenterPosition) {
       setOffsetY(scrollPosition);
@@ -25,31 +25,42 @@ const Hero = () => {
 
   const textStyle = {
     transform: `translateY(calc(100vh - ${offsetY}px))`,
-    transition: 'transform 0.1s linear', // Smooth transition
+    transition: 'transform 0.1s linear',
   };
 
   const containerStyle = {
-    backgroundImage: `url(${babyHands})`,
-    backgroundAttachment: 'fixed',
-    height: '150vh', // Ensure there's enough space to scroll
+    background: `url(${babyHands}) no-repeat`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    height: '100vh',
+    position: 'sticky',
+    top: '0',
+    overflow: 'hidden',
+  };
+
+  const contentStyle = {
+    height: '170vh', // 使页面足够长以触发滚动
+    overscrollBehavior: 'contain',  // 防止子内容滚动影响父背景
   };
 
   return (
-    <div style={containerStyle} className="bg-cover bg-center relative">
-      <div className='absolute top-0 left-100 right-0 p-4 text-center flex flex-col justify-center' style={textStyle}>
-        <h1 className='text-6xl font-bold py-4'>
-          让奇迹生辉
-        </h1>
-        <p className='text-xl sm:text-2xl md:text-4xl font-bold py-4'>
-          —— 您的私人助孕管家
-        </p>
-        <p className='text-xl md:text-2xl font-bold'>
-          坐拥全球顶尖生殖医生、律师、第三方生殖管理机构资源,
-          <br className="hidden md:block" />
-          20年专业经验，服务2000+财富家庭，
-          <br className="hidden md:block" />
-          管家式服务为您提供尊享体验。
-        </p>
+    <div style={contentStyle}>
+      <div style={containerStyle} className="center top h-screen relative">
+        <div className="absolute top-0 left-50 right-0 p-4 text-center flex flex-col justify-center" style={textStyle}>
+          <h1 className="text-6xl font-bold py-4">
+            让奇迹生辉
+          </h1>
+          <p className="text-xl sm:text-2xl md:text-4xl font-bold py-4">
+            —— 您的私人助孕管家
+          </p>
+          <p className="text-xl md:text-2xl font-bold">
+            坐拥全球顶尖生殖医生、律师、第三方生殖管理机构资源,
+            <br className="hidden md:block" />
+            20年专业经验，服务2000+财富家庭，
+            <br className="hidden md:block" />
+            管家式服务为您提供尊享体验。
+          </p>
+        </div>
       </div>
     </div>
   );
