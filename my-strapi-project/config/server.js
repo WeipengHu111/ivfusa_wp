ad89afee02a8a485030aa01cf55579058c0b63f1
WeipengHu111 <70785418+WeipengHu111@users.noreply.github.com>
2024-07-18 17:08:34 -0400

    config_server_update

--- my-strapi-project/config/server.js
+++ my-strapi-project/config/server.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
-  url: env("PUBLIC_URL", "http://localhost:1337"),
+  url: env("PUBLIC_URL", env("NODE_ENV") === "production" ? "https://ivfusa-init-wp.onrender.com" : "http://localhost:1337"),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'yourAdminJwtSecret'),
+    },
+  },
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['yourAppKey1', 'yourAppKey2']),
   },
 });
